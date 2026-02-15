acccda54b7b723bd9dc7fb6e93232cf51af833b3
Siddesh B <siddeshs532@gmail.com>
2026-02-15 17:11:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,7 +6,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBW7rCLSaPDGcANiSU4GO6wPd4LRLm7-nY",
   authDomain: "feedback-project-4c44e.firebaseapp.com",
   projectId: "feedback-project-4c44e",
-  storageBucket: "feedback-project-4c44e.appspot.com",
+  storageBucket: "feedback-project-4c44e.firebasestorage.app",
   messagingSenderId: "180336319356",
   appId: "1:180336319356:web:28b073f7a0c62c82b362f9",
   measurementId: "G-MC9C48MHSW"
