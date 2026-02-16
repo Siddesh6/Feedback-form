08d02740a7d6d6e0a07b2d33b6d6bc8ab48fac22
siddeshb.contact <siddeshb.contact@gmail.com>
2026-02-16 18:53:42 +0000

    addthe firebase.ts

---
+++ src/lib/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBW7rCLSaPDGcANiSU4GO6wPd4LRLm7-nY",
+  authDomain: "feedback-project-4c44e.firebaseapp.com",
+  projectId: "feedback-project-4c44e",
+  storageBucket: "feedback-project-4c44e.firebasestorage.app",
+  messagingSenderId: "180336319356",
+  appId: "1:180336319356:web:28b073f7a0c62c82b362f9",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
